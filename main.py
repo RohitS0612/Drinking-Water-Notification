c04f80831a50452286921f6fb047074242f4e903
Rohit Shetake <99246721+RohitS0612@users.noreply.github.com>
2024-07-04 16:44:22 +0530

    Update main.py
    upadted file icon

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def Water():
             title = " Now Water Time !",
             message = " 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
             
-            app_icon ="C:\\Users\\sheta\\OneDrive\\Desktop\\Practice Programe\\Drinking Water Notification\\drink-bottle.ico",
+            app_icon ="drink-bottle.ico",
             timeout=(15)
         )
         time.sleep(15*15)
